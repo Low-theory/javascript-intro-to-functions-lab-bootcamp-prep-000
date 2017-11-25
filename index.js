5edc3b22ed3fcc85d306a93d7629aa23361bfbcf
Andrew Echemendia <anderetxe1936@gmail.com>
2017-11-25 17:23:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(Hello) {
+  console.log('HELLO')
+}
+function logWhisper(hello) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  
+}
